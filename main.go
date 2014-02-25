3be33732ecac482056c18ec97a30523d98ef7d8c
nemunaire <pomercier@novaquark.com>
2014-02-25 12:25:41 +0100

    Add licence header

--- main.go
+++ main.go
@@ -1,3 +1,12 @@
+// sysinfo_influxdb by Novaquark
+//
+// To the extent possible under law, the person who associated CC0 with
+// sysinfo_influxdb has waived all copyright and related or neighboring rights
+// to sysinfo_influxdb.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 package main
 
 import (
